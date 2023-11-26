e59a2f5d66e73ed8beb16e9403ee1daeafc9636d
vikivoskoboinykarma <114704416+vikivoskoboinykarma@users.noreply.github.com>
2023-11-25 21:54:03 -0700

    update/color-switcher

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function startColorSwitcher() {
+  startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    changeBodyColor();
+  }, 1000);
+}
+
+function stopColorSwitcher() {
+  startBtn.disabled = false;
+
+  clearInterval(intervalId);
+}
+
+startBtn.addEventListener('click', startColorSwitcher);
+stopBtn.addEventListener('click', stopColorSwitcher);
